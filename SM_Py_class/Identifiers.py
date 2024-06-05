1c25d2161c31a006be8b2577c20ea72b1060f686
Chaitanya K <159884203+ChaitanyaKawale@users.noreply.github.com>
2024-06-05 11:54:31 +0530

    Add files via upload

---
+++ SM_Py_class/Identifiers.py
@@ -0,0 +1,3 @@
+import keyword #this imports all the keyword python has inbuilt
+print(keyword.kwlist)   #will print the list of keywords
+print(len(keyword.kwlist))  #gives the lenght(total number of usually a string) of keywords
